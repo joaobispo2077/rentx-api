cb3af72c4c03f3c1d6e9212fc88ae0eec595a1bf
João Bispo <joaobispo2077@gmail.com>
2021-08-28 20:39:54 -0300

    feat: add debug on the fly config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
 const app = express();
-
+app.use(express.json());
 const PORT = process.env.PORT || 3333;
 
-app.get('/', (_, response) => {
-  return response.json({ message: 'Hello world!' });
+app.get('/', (request, response) => {
+  const { name } = request.query;
+  return response.json({ message: name });
 });
 
 app.listen(PORT, () => console.log(`Server is running at por ${PORT}`));
