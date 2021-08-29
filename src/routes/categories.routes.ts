4b95870148c8ccc9fea2aa65ba4ce041a430e533
João Bispo <joaobispo2077@gmail.com>
2021-08-29 10:42:24 -0300

    refactor: decoupling list categories use case

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/categories', (request, response) =>
   createCategoryController.handle(request, response),
 );
 
-categoriesRoutes.get('/categories', (request, response) => {
-  const categories = categoriesRepository.list();
-
-  return response.json(categories);
-});
+categoriesRoutes.get('/categories', (request, response) =>
+  listCategoriesController.handle(request, response),
+);
 
 export { categoriesRoutes };
