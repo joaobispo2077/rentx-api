1cd9cbfc249cf01044f3fc12b672066608eec277
João Bispo <joaobispo2077@gmail.com>
2021-09-02 21:28:39 -0300

    feat: add migration create categories

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,5 @@
 require('dotenv/config');
+const path = require('path');
 
 module.exports = {
   type: 'postgres',
@@ -7,4 +8,8 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
+  migrations: [path.join('.', 'src', 'database', 'migrations', '*.ts')],
+  cli: {
+    migrationsDir: path.join('.', 'src', 'database', 'migrations'),
+  }
 }
