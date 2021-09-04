59620e0a70e5a916453e5454b9df16ba192bf73d
João Bispo <joaobispo2077@gmail.com>
2021-09-03 21:12:57 -0300

    refactor: migrate list categories to async flow

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import createCategoryController from '../modules/cars/useCases/createCategory';
-import { importCategoryController } from '../modules/cars/useCases/importCategory';
-import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+// import { importCategoryController } from '../modules/cars/useCases/importCategory';
+import listCategoriesController from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
 
@@ -16,11 +16,11 @@ categoriesRoutes.post('/', (request, response) =>
 );
 
 categoriesRoutes.get('/', (request, response) =>
-  listCategoriesController.handle(request, response),
+  listCategoriesController().handle(request, response),
 );
 
-categoriesRoutes.post('/import', upload.single('file'), (request, response) =>
-  importCategoryController.handle(request, response),
-);
+// categoriesRoutes.post('/import', upload.single('file'), (request, response) =>
+//   importCategoryController.handle(request, response),
+// );
 
 export { categoriesRoutes };
