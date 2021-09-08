cbb80e22bc27424f52fe14e6dcba6317ab266bcd
João Bispo <joaobispo2077@gmail.com>
2021-09-07 21:07:06 -0300

    feat: add CategoriesRepository Mock

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,31 @@
+import { Category } from '../../entities/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from '../ICategoriesRepository';
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  private categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category | undefined> {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+
+  async list(): Promise<Category[]> {
+    const categories = this.categories;
+    return categories;
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<Category> {
+    const category = new Category();
+    category.name = name;
+    category.description = description;
+
+    this.categories.push(category);
+
+    return category;
+  }
+}
+
+export { CategoriesRepositoryInMemory };
