2ef02c51bc97b5e2aa4396f0f55334e2075c904a
João Bispo <joaobispo2077@gmail.com>
2021-09-07 21:07:25 -0300

    test: should be able to create a new category

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,41 @@
+import { AppError } from '../../../../errors/AppError';
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+describe('UseCase - Create Category', () => {
+  let createCategoryUseCase: CreateCategoryUseCase;
+  let categoriesRepository: CategoriesRepositoryInMemory;
+
+  beforeEach(() => {
+    categoriesRepository = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+  });
+
+  it('should be able to create a new category', async () => {
+    const category = {
+      name: 'Car category',
+      description: 'Car description',
+    };
+
+    const newCategory = await createCategoryUseCase.execute(category);
+
+    expect(newCategory).toEqual(expect.objectContaining(category));
+  });
+
+  it('should not be able to create a category that already exists', async () => {
+    const category = {
+      name: 'Car-error-category',
+      description: 'Car description',
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    await expect(
+      createCategoryUseCase.execute(category),
+    ).rejects.toBeInstanceOf(AppError);
+
+    await expect(createCategoryUseCase.execute(category)).rejects.toEqual(
+      new AppError('Category already exists.'),
+    );
+  });
+});
