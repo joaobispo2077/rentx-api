56f9b2b0d381708eee59dd158530d101478d1671
João Bispo <joaobispo2077@gmail.com>
2021-09-12 17:23:48 -0300

    test: should be able to filter cars by category id

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -63,4 +63,23 @@ describe('Use case - List cars', () => {
 
     expect(cars.length).toBe(1);
   });
+
+  it('should be able to filter cars by category id', async () => {
+    const car = {
+      name: 'T5',
+      brand: 'Tesla',
+      description: 'Carro com bateria',
+      daily_rate: 60,
+      license_plate: 'ABC-1236',
+      fine_amount: 60,
+      category_id: 'be81e7b3-a9b0-453d-ac92-36909c7361bf',
+    };
+    await carsRepository.create(car);
+
+    const cars = await listCarUseCase.execute({
+      category_id: car.category_id,
+    });
+
+    expect(cars.length).toBe(1);
+  });
 });
