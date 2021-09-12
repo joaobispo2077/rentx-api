7412c61040449507b8d1980a95277c4894e1c3e1
João Bispo <joaobispo2077@gmail.com>
2021-09-12 17:22:51 -0300

    test: should be able to filter cars by name

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -44,4 +44,23 @@ describe('Use case - List cars', () => {
 
     expect(cars.length).toBe(1);
   });
+
+  it('should be able to filter cars by name', async () => {
+    const car = {
+      name: 'T5',
+      brand: 'Tesla',
+      description: 'Carro com bateria',
+      daily_rate: 60,
+      license_plate: 'ABC-1236',
+      fine_amount: 60,
+      category_id: 'be81e7b3-a9b0-453d-ac92-36909c7361bf',
+    };
+    await carsRepository.create(car);
+
+    const cars = await listCarUseCase.execute({
+      name: car.name,
+    });
+
+    expect(cars.length).toBe(1);
+  });
 });
