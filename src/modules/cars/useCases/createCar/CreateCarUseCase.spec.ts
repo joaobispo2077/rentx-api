086eeda03011f684d59b60dd61dcc2024205c38e
João Bispo <joaobispo2077@gmail.com>
2021-09-12 15:15:07 -0300

    test: should be able to create a car with avaiability as true by default

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -45,9 +45,20 @@ describe('Use case - Create Car', () => {
     );
   });
 
-  // it('should be able to create a car with free avaiability by default', async () => {
-  //   //
-  // });
+  it('should be able to create a car with avaiability as true by default', async () => {
+    const car = {
+      name: 'Uno',
+      brand: 'Fiat',
+      description: 'Carro de luxo',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      category_id: 'SUV',
+    };
+
+    const newCar = await createCarUseCase.execute(car);
+    expect(newCar).toHaveProperty('available', true);
+  });
 
   // it('should not be able a user non admin to create a car', async () => {
   //   //
