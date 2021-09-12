fa5196fa74ce4be26c0f509cfb2e9c1f6e44448d
João Bispo <joaobispo2077@gmail.com>
2021-09-12 15:08:02 -0300

    test:  should not be able to create a car with a license_plate that already exists

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateCarUseCase } from './CreateCarUseCase';
 
@@ -6,6 +7,10 @@ const carsRepository = new CarsRepositoryInMemory();
 const createCarUseCase = new CreateCarUseCase(carsRepository);
 
 describe('Use case - Create Car', () => {
+  beforeEach(async () => {
+    await carsRepository.clear();
+  });
+
   it('shoud be able to create a new car', async () => {
     const car = {
       name: 'Uno',
@@ -21,4 +26,30 @@ describe('Use case - Create Car', () => {
 
     expect(newCar).toEqual(expect.objectContaining(car));
   });
+
+  it('should not be able to create a car with a license plate that already exists', async () => {
+    const car = {
+      name: 'Uno',
+      brand: 'Fiat',
+      description: 'Carro de luxo',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      category_id: 'SUV',
+    };
+
+    await createCarUseCase.execute(car);
+
+    await expect(createCarUseCase.execute(car)).rejects.toEqual(
+      new AppError('Car already exists.', 409),
+    );
+  });
+
+  // it('should be able to create a car with free avaiability by default', async () => {
+  //   //
+  // });
+
+  // it('should not be able a user non admin to create a car', async () => {
+  //   //
+  // });
 });
