d5872a506158d4c8591bc4f45eb063777ebac808
João Bispo <joaobispo2077@gmail.com>
2021-09-13 18:05:59 -0300

    test: should not be able to create a new rental if there is another open rental to the same user

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from '@modules/rentals/infra/typeorm/repositories/in-memory/RentalsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateRentalUseCase } from './CreateRentalUseCase';
 
@@ -18,5 +19,21 @@ describe('Use case - Create Rental', () => {
     });
 
     expect(rental).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new rental if there is another open rental to the same user', async () => {
+    await createRentalUseCase.execute({
+      user_id: 'user_id',
+      car_id: 'car_id1',
+      expected_return_date: new Date(),
+    });
+
+    await expect(
+      createRentalUseCase.execute({
+        user_id: 'user_id',
+        car_id: 'car_id2',
+        expected_return_date: new Date(),
+      }),
+    ).rejects.toEqual(new AppError('User is not available', 409));
   });
 });
