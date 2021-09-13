1c71831e45464dbb44b984d87369c541ccf7fd75
João Bispo <joaobispo2077@gmail.com>
2021-09-13 18:02:00 -0300

    feat: add method create into RentalsRepositoryInMemory

--- src/modules/rentals/infra/typeorm/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/infra/typeorm/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -1,7 +1,26 @@
+import { ICreateRentalDTO } from '@modules/rentals/dtos/ICreateRentalDTO';
+
 import { Rental } from '../../entities/Rental';
 import { IRentalsRepository } from '../IRentalsRepository';
 
 class RentalsRepositoryInMemory implements IRentalsRepository {
+  async create({
+    car_id,
+    user_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+    Object.assign(rental, {
+      car_id: car_id,
+      user_id: user_id,
+      start_date: new Date(),
+      end_date: null,
+      expected_return_date: expected_return_date,
+    });
+
+    this.rentals.push(rental);
+    return rental;
+  }
   private rentals: Rental[] = [];
 
   async findOpenRentalByCarId(car_id: string): Promise<Rental | undefined> {
