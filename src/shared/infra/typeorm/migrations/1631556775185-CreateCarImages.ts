9613aaa3632bbf7efda8698731368dd5f52788bc
João Bispo <joaobispo2077@gmail.com>
2021-09-13 15:29:28 -0300

    feat: add migration create car images

---
+++ src/shared/infra/typeorm/migrations/1631556775185-CreateCarImages.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCarImages1631556775185 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'cars_image',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'image_name',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'car_id',
+            type: 'uuid',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKCarImage',
+            referencedTableName: 'cars',
+            referencedColumnNames: ['id'],
+            columnNames: ['car_id'],
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cars_image');
+  }
+}
