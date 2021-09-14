6db6cf7904a9d01bea59bf5b6fb803f99db6b388
João Bispo <joaobispo2077@gmail.com>
2021-09-14 16:50:07 -0300

    feat: add create rental controller

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { authenticateRoutes } from './authenticate.routes';
 import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
+import { rentalsRoutes } from './rentals.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
 
@@ -11,6 +12,7 @@ const routes = Router();
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
 routes.use('/cars', carsRoutes);
+routes.use('/rentals', rentalsRoutes);
 
 routes.use('/users', usersRoutes);
 routes.use('/sessions', authenticateRoutes);
