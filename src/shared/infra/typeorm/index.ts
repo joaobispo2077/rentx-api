3c151cca080ea46988c236a4b8d51af514afaf7d
João Bispo <joaobispo2077@gmail.com>
2021-09-15 12:11:10 -0300

    chore: add log when connect with pg database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,5 +2,9 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
+  console.info(
+    'creating connection with database: ',
+    process.env.POSTGRES_DATABASE,
+  );
   return createConnection(defaultOptions);
 };
