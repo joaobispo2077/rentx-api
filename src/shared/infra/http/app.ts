40a71a3e78234cce4b10545581e4ab3498653d13
João Bispo <joaobispo2077@gmail.com>
2021-09-15 10:29:30 -0300

    refactor: separate app from server starting

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,26 @@
+require('dotenv/config');
+
+import 'reflect-metadata';
+import 'express-async-errors';
+
+import '@shared/containers';
+import '@shared/containers/providers';
+
+import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+
+import createConnection from '@shared/infra/typeorm';
+
+import swaggerFile from '../../../swagger.json';
+import { handleError } from './middlewares/handleError';
+import { routes } from './routes';
+
+createConnection();
+const app = express();
+app.use(express.json());
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+app.use(routes);
+app.use(handleError);
+
+export { app };
