6e69bbb289cc52064a56a1e48b50e44f781615e4
João Bispo <joaobispo2077@gmail.com>
2021-09-16 11:55:14 -0300

    feat: add devolution rental controller with endpoint

---
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { DevolutionRentalUseCase } from './DevolutionRentalUseCase';
+
+class DevolutionRentalController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id: userId } = request.user;
+    const { rentalId } = request.params;
+
+    const devolutionRentalUseCase = container.resolve(DevolutionRentalUseCase);
+
+    const rental = await devolutionRentalUseCase.execute({
+      userId,
+      rentalId,
+    });
+
+    return response.json(rental);
+  }
+}
+
+export { DevolutionRentalController };
--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
+import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
@@ -8,5 +9,12 @@ const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 rentalsRoutes.post('/', ensureAuthenticated, createRentalController.handle);
+
+const devolutionRentalController = new DevolutionRentalController();
+rentalsRoutes.put(
+  '/:rentalId/devolution',
+  ensureAuthenticated,
+  devolutionRentalController.handle,
+);
 
 export { rentalsRoutes };
