b2d54aa89d837678e5d2cb925b41b7425ae56d57
João Bispo <joaobispo2077@gmail.com>
2021-09-17 17:06:46 -0300

    feat: add migrations from user refresh token

---
+++ src/shared/infra/typeorm/migrations/1631908954778-CreateUsersRefreshToken.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsersRefreshToken1631908954778
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users_refresh_token',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'expires_date',
+            type: 'timestamp',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKUserRefreshToken',
+            columnNames: ['user_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users_refresh_token');
+  }
+}
