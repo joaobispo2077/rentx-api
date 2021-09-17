5132d381e96df6bf26298025dcf2971e4d88003f
João Bispo <joaobispo2077@gmail.com>
2021-09-17 18:59:31 -0300

    feat: add refresh session route

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import { AuthenticateUserController } from '@modules/accounts/useCases/authenticateUser/AuthenticateUserController';
+import { RefreshTokenController } from '@modules/accounts/useCases/refreshToken/RefreshTokenController';
 
 const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
 authenticateRoutes.post('/', authenticateUserController.handle);
+
+const refreshTokenController = new RefreshTokenController();
+authenticateRoutes.post('/:email/refresh', refreshTokenController.handle);
 
 export { authenticateRoutes };
