00e3e8015a7b81a61eea4f2feca442bff8dc79f3
João Bispo <joaobispo2077@gmail.com>
2021-09-18 19:26:27 -0300

    feat: add reset password endpoint

--- src/shared/infra/http/routes/passwords.routes.ts
+++ src/shared/infra/http/routes/passwords.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
+import { ResetPasswordUserController } from '@modules/accounts/useCases/resetPassword/ResetPasswordUserController';
 import { SendForgotPasswordMailController } from '@modules/accounts/useCases/sendForgotPassword/SendForgotPasswordMailController';
 
 const passwordsRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
 passwordsRoutes.post('/:email/forgot', sendForgotPasswordMailController.handle);
+
+const resetPasswordUserController = new ResetPasswordUserController();
+passwordsRoutes.post('/:email/reset', resetPasswordUserController.handle);
 
 export { passwordsRoutes };
