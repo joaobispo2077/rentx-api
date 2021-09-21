ad960c9381b96071872a712abaf715f46c8e1485
João Bispo <joaobispo2077@gmail.com>
2021-09-21 11:29:28 -0300

    feat: add endpoint to recover user data

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { ShowUserProfileController } from '@modules/accounts/useCases/showUserProfileUseCase/ShowUserProfileController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
@@ -12,7 +13,9 @@ const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const showUserProfileController = new ShowUserProfileController();
 
+usersRoutes.get('/:id', ensureAuthenticated, showUserProfileController.handle);
 usersRoutes.post('/', createUserController.handle);
 usersRoutes.patch(
   '/avatar',
