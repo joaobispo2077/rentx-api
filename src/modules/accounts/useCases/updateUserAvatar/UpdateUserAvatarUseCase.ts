ebc8070b5570f9991e923759871f6e7bd9fe0192
João Bispo <joaobispo2077@gmail.com>
2021-09-21 10:42:12 -0300

    refactor: add StorageProvider into update user avatar use case

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import { AppError } from '@errors/AppError';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IStorageProvider } from '@shared/containers/providers/StorageProvider/IStorageProvider';
 import { deleteFile } from '@utils/file';
 
 interface IPayload {
@@ -14,6 +15,8 @@ class UpdateUserAvatarUseCase {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
   async execute({ userId, avatar }: IPayload): Promise<void> {
     const user = await this.usersRepository.findById(userId);
@@ -21,10 +24,11 @@ class UpdateUserAvatarUseCase {
     if (!user) {
       throw new AppError('User not found');
     }
-
     if (user.avatar) {
-      await deleteFile(`./tmp/avatar/${user.avatar}`);
+      await this.storageProvider.delete(user.avatar, 'avatar');
     }
+
+    await this.storageProvider.save(avatar, 'avatar');
 
     user.avatar = avatar;
 
