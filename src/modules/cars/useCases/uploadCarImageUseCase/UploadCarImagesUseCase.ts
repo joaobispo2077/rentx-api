939dfb3406d36a8cacb581a42420ed4cc9d8e2f1
João Bispo <joaobispo2077@gmail.com>
2021-09-21 11:17:21 -0300

    refactor: add StorageProvider into upload car image use case

--- src/modules/cars/useCases/uploadCarImageUseCase/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImageUseCase/UploadCarImagesUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import { CarImage } from '@modules/cars/infra/typeorm/entities/CarImage';
 import { ICarsImageRepository } from '@modules/cars/repositories/ICarsImageRepository';
+import { IStorageProvider } from '@shared/containers/providers/StorageProvider/IStorageProvider';
 
 interface IPayload {
   car_id: string;
@@ -13,10 +14,13 @@ class UploadCarImagesUseCase {
   constructor(
     @inject('CarsImageRepository')
     private carsImageRepository: ICarsImageRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
   async execute({ car_id, image_names }: IPayload): Promise<CarImage[]> {
     const createdImages = await Promise.all(
       image_names.map(async (image_name) => {
+        await this.storageProvider.save(image_name, 'cars');
         return await this.carsImageRepository.create({
           car_id,
           image_name,
