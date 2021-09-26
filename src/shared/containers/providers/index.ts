beeb6589aa2f0777eb708e785048e622e075ab40
João Bispo <joaobispo2077@gmail.com>
2021-09-25 21:02:29 -0300

    fix: remove unused imports from providers container

--- src/shared/containers/providers/index.ts
+++ src/shared/containers/providers/index.ts
@@ -1,6 +1,3 @@
-import { container } from 'tsyringe';
-
-import { IDateProvider } from './DateProvider/IDateProvider';
 import './MailProvider';
 import './StorageProvider';
 import './DateProvider';
