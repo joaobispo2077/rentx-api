382f808c0eb7d4d15c7040ed86149b1f8b69f283
João Bispo <joaobispo2077@gmail.com>
2021-09-26 14:07:02 -0300

    feat: add sentry to tracing error metrics

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -11,6 +11,8 @@ import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 
 import upload from '@config/upload';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import createConnection from '@shared/infra/typeorm';
 
@@ -22,19 +24,36 @@ createConnection();
 
 const app = express();
 
+console.log(process.env.SENTRY_DSN);
+app.use(rateLimiter); // before tracing errors
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 app.use(
   cors({
     origin: '*',
   }),
 );
-app.use(rateLimiter);
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
+
 app.use(handleError);
 
 export { app };
